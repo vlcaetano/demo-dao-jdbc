cc1837f302aa22ca1f3d67886909e02aa31af3de
Vitor Caetano <vitorlc95@gmail.com>
2020-05-26 15:13:15 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -40,6 +40,10 @@ public class Program {
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Update completed");
+		
+		System.out.println("\n==== TEST 6: seller delete ====");
+		sellerDao.deleteById(9);
+		System.out.println("Delete completed");
 	}
 
 }
